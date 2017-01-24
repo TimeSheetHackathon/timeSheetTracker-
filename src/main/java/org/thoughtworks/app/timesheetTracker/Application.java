a6b50a876dd7b8348ef62e7c4b28d47006fada99
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2017-01-24 15:25:06 +0530

    Prasenjit | Removing unused imports

--- src/main/java/org/thoughtworks/app/timesheetTracker/Application.java
+++ src/main/java/org/thoughtworks/app/timesheetTracker/Application.java
@@ -1,16 +1,9 @@
 package org.thoughtworks.app.timesheetTracker;
 
-import java.util.HashMap;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.thoughtworks.app.timesheetTracker.models.MissingTimeSheetData;
-import org.thoughtworks.app.timesheetTracker.repository.S3Client;;
+
+;
 
 @SpringBootApplication
 public class Application {
