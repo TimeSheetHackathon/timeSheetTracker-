c09995a5e8ea39a5299f8c86173fab9588b2f0ec
piolAtif <spreeti@thoughtworks.com>
2017-02-01 11:03:10 +0530

    [Preeti/Prasenjit] +Refactored S3Client parseEmployeeData and fetchFileFromAWS
    What changes have you made?
    
    * Made parseEmployeeData and fetchFileFromAws return function type
    * Removed parseEmployeeData code from fetchFileFromAWS
    * Changed logic of fetching file and parse into employee in getTimeSheet methods
    
    Why do we need this change?
    
    * To make fetchFileFromAWS and parseEmployeeData to flexible
    
    What can this change impact?
    
    * We can add new method with fetchFileFromAWS

--- src/main/java/org/thoughtworks/app.timesheetTracker.repository/S3Client.java
+++ src/main/java/org/thoughtworks/app.timesheetTracker.repository/S3Client.java
@@ -1,7 +1,9 @@
 package org.thoughtworks.app.timesheetTracker.repository;
 
+import com.amazonaws.services.dynamodbv2.xspec.S;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
@@ -20,6 +22,7 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.lang.String.valueOf;
@@ -27,61 +30,74 @@ import static java.lang.String.valueOf;
 
 @Repository
 
-public class
-S3Client {
+public class S3Client {
 
-    @Autowired
-    private Environment env;
+  @Autowired
+  private Environment env;
 
-    @Autowired
-    private AmazonS3Client amazonS3Client;
+  @Autowired
+  private AmazonS3Client amazonS3Client;
 
-    private final static Logger logger = LoggerFactory.getLogger(TimeSheetTrackerController.class);
+  private final static Logger logger = LoggerFactory.getLogger(TimeSheetTrackerController.class);
 
-    @Cacheable("timeSheetFileForLastWeek")
-    public List<MissingTimeSheetData> getTimeSheetFileForLastWeek() {
-        return fetchFileFromAWS(env.getProperty("cloud.aws.weekly.timesheet.file.prefix"));
-    }
+  @Cacheable("timeSheetFileForLastWeek")
+  public List<MissingTimeSheetData> getTimeSheetFileForLastWeek() {
+    final String filePrefix = env.getProperty("cloud.aws.weekly.timesheet.file.prefix");
+    return fetchFileFromAWS().andThen(parseEmployeeData()).apply(filePrefix);
+  }
 
-    @Cacheable("employeesNamesOfMissingTimeSheet")
-    public List<MissingTimeSheetData> getTimeSheetFileForProjectLastWeek() {
-        return fetchFileFromAWS(env.getProperty("cloud.aws.weekly.project.timeseet.mising.file.prefix"));
-    }
+  @Cacheable("employeesNamesOfMissingTimeSheet")
+  public List<MissingTimeSheetData> getTimeSheetFileForProjectLastWeek() {
+    final String filePrefix =
+        env.getProperty("cloud.aws.weekly.project.timeseet.mising.file.prefix");
+    return fetchFileFromAWS().andThen(parseEmployeeData()).apply(filePrefix);
+  }
 
-    private List<MissingTimeSheetData> fetchFileFromAWS(String filePrefix) {
-        logger.info("Fetching file from aws");
+  private Function<String, S3ObjectInputStream> fetchFileFromAWS() {
+    return filePrefix -> {
+      logger.info("Fetching file from aws");
 
-        final S3Object s3Object = amazonS3Client.getObject(env.getProperty("cloud.aws.timesheet.bucket.name"),
-                                String.format(filePrefix, getPreviousWeek()));
-        try {
-            return parseEmployeeData(s3Object.getObjectContent());
-        } catch (Exception e) {
-            logger.info("Fetching file failed from aws");
-            logger.info(e.getMessage());
-            return Collections.emptyList();
-        }
-    }
+      final S3Object s3Object =
+          amazonS3Client.getObject(env.getProperty("cloud.aws.timesheet.bucket.name"),
+              String.format(filePrefix, getPreviousWeek()));
+      try {
+        return s3Object.getObjectContent();
+      } catch (Exception e) {
+        logger.info("Fetching file failed from aws");
+        logger.info(e.getMessage());
+      }
+      return null;
+    };
+  }
 
 
-    private List<MissingTimeSheetData> parseEmployeeData(InputStream input) throws IOException {
-        final ObjectMapper mapper = new ObjectMapper();
+  private Function<InputStream, List<MissingTimeSheetData>> parseEmployeeData() {
+    return input -> {
+      final ObjectMapper mapper = new ObjectMapper();
+      try {
         return new JSONArray(IOUtils.toString(input)).toList().stream()
-                .map(e -> {
-                    Map timeSheetDataMap = mapper.convertValue(e, Map.class);
+            .map(e -> {
+              Map timeSheetDataMap = mapper.convertValue(e, Map.class);
 
-                    return MissingTimeSheetData.builder()
-                            .country(valueOf(timeSheetDataMap.getOrDefault("country", "")).toUpperCase())
-                            .employeeId(valueOf(timeSheetDataMap.getOrDefault("id", "")).toUpperCase())
-                            .workingLocation(valueOf(timeSheetDataMap.getOrDefault("working-office", "")).toUpperCase())
-                            .projectName(valueOf(timeSheetDataMap.getOrDefault("project-name", "")).toUpperCase())
-                            .employeeName(valueOf(timeSheetDataMap.getOrDefault("name","")))
-                            .build();
-                })
-                .collect(Collectors.toList());
-    }
+              return MissingTimeSheetData.builder()
+                  .country(valueOf(timeSheetDataMap.getOrDefault("country", "")).toUpperCase())
+                  .employeeId(valueOf(timeSheetDataMap.getOrDefault("id", "")).toUpperCase())
+                  .workingLocation(valueOf(timeSheetDataMap.getOrDefault("working-office", "")).toUpperCase())
+                  .projectName(valueOf(timeSheetDataMap.getOrDefault("project-name", "")).toUpperCase())
+                  .employeeName(valueOf(timeSheetDataMap.getOrDefault("name", "")))
+                  .build();
+            })
+            .collect(Collectors.toList());
+      } catch (IOException e) {
+        logger.info(e.getMessage());
+      }
+      return Collections.emptyList();
+    };
+  }
 
-    private int getPreviousWeek() {
-        return Calendar.getInstance().get(Calendar.WEEK_OF_YEAR)-1;
-    }
+  private int getPreviousWeek() {
+    return Calendar.getInstance().get(Calendar.WEEK_OF_YEAR) - 1;
+  }
+
 }
 
