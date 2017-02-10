c11535ff1ab161e0d22330378ad995abb7d10e86
piolAtif <spreeti@thoughtworks.com>
2017-02-10 11:24:56 +0530

    [Preeti/Prasenjit] +Added encrypted password for accessing mongodb

--- src/main/java/org/thoughtworks/app/timeSheetTracker/Application.java
+++ src/main/java/org/thoughtworks/app/timeSheetTracker/Application.java
@@ -1,12 +1,14 @@
 package org.thoughtworks.app.timeSheetTracker;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication
+@EnableEncryptableProperties
 public class Application {
 
 	public static void main(String[] args) {
